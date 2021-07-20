46f4af68e9f1a5cb0772ddb65b942701f1e56786
itspacchu <37984032+itspacchu@users.noreply.github.com>
2021-07-21 01:41:32 +0530

    now it works

--- scraper.py
+++ scraper.py
@@ -1,40 +1,101 @@
 import numpy as np
-import urllib3 as uli
 from selenium import webdriver
+from selenium.common import exceptions as SeleniumExcept
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import TimeoutException
 import os
-import requests
-from PIL import Image
-import pytesseract
-from Screenshot import Screenshot_Clipping
-import cv2
+import time
 
-#baking webpage captcha constants below
-left = 426
-top = 428
-bottom = 444
-right = 588
-randbirthday = "2001-09-15"
-#ENTER RNO
-rno = ""
+def initSeleniumDriver():
+    chromedriver = "chromedriver.exe"
+    os.environ["webdriver.chrome.driver"] = chromedriver
+    driver = webdriver.Chrome(chromedriver)
+    return driver
+
+def isItValidRollNumber(RollNumber):
+    checks = 0
+    if(len(RollNumber) != 10):
+        return False
+    try:
+        int(RollNumber[:2])
+    except ValueError:
+        return False
+    try:
+        int(RollNumber[-3:], base=16)
+    except ValueError:
+        return False
+
+    return True
+
+def JntuResultScraper(RollNumber, ExamCode="1454", _type_="intgrade"):
+    resultDict = {}
+    randbirthday = "2001-11-11"
+    if(isItValidRollNumber(RollNumber)):
+        pass
+    else:
+        raise(ValueError,"Roll-Number not Valid")
+    jntuResultUrl = f"http://results.jntuh.ac.in/jsp/SearchResult.jsp?degree=btech&examCode={ExamCode}&etype=r17&type={_type_}"
+    driver = initSeleniumDriver()
+    
+    driver.get(jntuResultUrl)
+    time.sleep(0.1)
+    rollNoField  = driver.find_element_by_id('htno')
+    time.sleep(0.1)
+    rollNoField.click()
+    rollNoField.send_keys(RollNumber)
+    time.sleep(0.1)
+
+    datePicker = driver.find_element_by_id("datepicker")
+    datePicker.send_keys(randbirthday)
+    time.sleep(0.1)
+    driver.find_element_by_xpath( "/html/body/form/div/table/tbody/tr[2]/td[1]").click()
+    time.sleep(0.1)
+
+    captchaTxt = driver.find_element_by_id('txtCaptcha').get_attribute('value')
+    time.sleep(0.1)
+    driver.find_element_by_id("txtInput").send_keys(captchaTxt)
+    time.sleep(0.1)
+    driver.find_element_by_xpath("//*[@id=\"myForm\"]/div/table/tbody/tr[5]/td[3]/input").click()
+    
+    time.sleep(2)
+    try:
+        myElem = WebDriverWait(driver, 3).until(EC.presence_of_element_located((By.TAG_NAME, 'img')))
+        print("Loaded")
+    except TimeoutException:
+        raise(TimeoutException,"Took took much time to load")
+    # result table iterator
+    ResEndFlag = True
+    rowIter = 1
+    while(ResEndFlag):
+        try:
+            currentCol = []
+            for colIter in range(1,8):            
+                try:
+                    val = driver.find_element_by_xpath(f"/html/body/form/div[1]/table/tbody/tr[{rowIter}]/td[{colIter}]/h4/b").text
+                except:
+                    val = driver.find_element_by_xpath(f"/html/body/form/div[1]/table/tbody/tr[{rowIter}]/td[{colIter}]/b").text
+                currentCol.append(val)
+            rowIter+=1
+            resultDict[currentCol[0]] = {
+                'SUB_NAME':currentCol[1],
+                'INTERNAL':currentCol[2],
+                'EXTERNAL':currentCol[3],
+                'TOTAL':currentCol[4],
+                'GRADE':currentCol[5],
+                'CREDIT':currentCol[6]
+            }
+            print('\n')
+        except SeleniumExcept.NoSuchElementException as e:
+            print(e)
+            break
+    
+    return resultDict
+
+if __name__ == "__main__":
+    resultDict = JntuResultScraper("18AG1A0401")
+    print(resultDict)
 
 
-ob=Screenshot_Clipping.Screenshot()
-chromedriver = "D:\\PyStuff\\SomeRandomGizmos\\chromedriver.exe"
-os.environ["webdriver.chrome.driver"] = chromedriver
-driver = webdriver.Chrome(chromedriver)
-#driver.get("http://epayments.jntuh.ac.in/results") got some weird tag issues here
-
-#DIRECT LINK FOR R18 2-1 Results
-driver.get("http://epayments.jntuh.ac.in/results/jsp/SearchResult.jsp?degree=btech&amp;examCode=1391&amp;etype=r17&amp;type=grade17")
-
-driver.find_element_by_name("htno").send_keys(rno)
-elem = driver.find_element_by_id('txtCaptcha')
-img_url=ob.full_Screenshot(driver, save_path=r'.', image_name='testscr.png') 
-text = pytesseract.image_to_string(Image.open(r"D:\\PyStuff\\SomeRandomGizmos\\testscr.png").crop( ( left, top, right, bottom ) ))
-driver.find_element_by_id("txtInput").send_keys(text)
-driver.find_element_by_id("datepicker").send_keys(randbirthday)
-print("\nPredicted Captcha: ",text)
-#Button1
-driver.find_element_by_xpath("//*[@id=\"myForm\"]/div/table/tbody/tr[5]/td[3]/input").click()
-
-#for now it just goes to results page directly
+    
