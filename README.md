b3ebe9406af9efe1d9b79a2056ac13d89c36bc27
leojfrancis <leojfrancis.now@gmail.com>
2021-07-21 20:18:09 +0530

    made it into class. added user details

--- README.md
+++ README.md
@@ -1,11 +1,14 @@
 # jntu-scraper
-results page scraping of jntuh website
-
-requires #Selenium
-``` pip install selenium ```
-
-Download Webdriver (Chrome) / For Firefox GeckoDriver Modify the function
-``` https://chromedriver.storage.googleapis.com/index.html ```
+Results page scraping of jntuh website
 
 ![image](https://user-images.githubusercontent.com/37984032/126389335-d9700b97-5c6d-4af8-ad1f-48857a4c5bb7.png)
 
+## *Depricated*
+> The Following is depricated, the New updates do not require Selenium or Web driver it uses Bf4. ***Do not follow this for the new version***
+
+Required *Selenium* 
+```
+pip install selenium 
+```
+
+[Download Webdriver (Chrome) / For Firefox GeckoDriver Modify the function](https://chromedriver.storage.googleapis.com/index.html)
--- scraper.py
+++ scraper.py
@@ -1,89 +1,114 @@
 from bs4 import BeautifulSoup
 import requests
 
-def GradeToPoint(Grade:str)->int:
-    if(Grade == "O"):
-        return 10
-    elif(Grade == 'A+'):
-        return 9
-    elif(Grade == 'A'):
-        return 8
-    elif(Grade == 'B+'):
-        return 7
-    elif(Grade == 'B'):
-        return 6
-    elif(Grade == 'C'):
-        return 5
-    else:
-        return 0
 
-def GPACalculator(result:dict):
-    total_credit = 0
-    gpa_accum = 0
-    for iter in range(0,len(result)):
-        gpa_accum += GradeToPoint(result[iter]['GRADE'])*float(result[iter]['CREDIT'])
-        total_credit += float(result[iter]['CREDIT']) 
-    return gpa_accum/total_credit
+class JNTUResult:
+    SERVERS = ['http://results.jntuh.ac.in', 'http://202.63.105.184/results']
 
-def isItValidRollNumber(RollNumber):
-    checks = 0
-    if(len(RollNumber) != 10):
-        return False
-    try:
-        int(RollNumber[:2])
-    except ValueError:
-        return False
-    try:
-        int(RollNumber[-3:], base=16)
-    except ValueError:
-        return False
+    def __init__(self, rollNum: str, examCode: int, **kwargs):
+        self.user = None
+        self.rollNum = self.isValidRollNumber(rollNum)
+        self.examCode = str(examCode)
+        self.degree = kwargs.get('degree', "btech")  # Defaults to btech
+        self.eType = kwargs.get('eType', "r17")  # Defaults to R18 results
+        # Defaults to Sem results
+        self.out_type = kwargs.get('out_type', "intgrade")
 
-    return True
+        self.url = self.get_url()
 
+    def __call__(self):
+        self.result = self.JNTUResultAPI()
+        self.sgpa = self.SGPACalculator(self.result)
+        return {
+            'user': self.user,
+            'result': self.result,
+            'sgpa': self.sgpa
+        }
 
-def JNTUResultAPI(RollNumber, ExamCode="1454", etype='r17', _type_="intgrade", server="results.jntuh.ac.in"):
-    resultDict = {}
-    randbirthday = "2001-11-11"
-    if(isItValidRollNumber(RollNumber)):
-        pass
-    else:
-        raise(ValueError, "Roll-Number not Valid")
-    jntuResultUrl = f"http://results.jntuh.ac.in/resultAction?degree=btech&examCode={ExamCode}&etype={etype}&type=intgrade&result=null&grad=null&htno={RollNumber}#"
-    payload = {}
-    headers = {
-        'Cookie': 'JSESSIONID=464656A62D7F6AFF4257BFAA544E0A84; cookiesession1=2D2443B1D1ZYJTB8GOLAEVRLKJTO4517'
-    }
-    response = requests.request("POST", jntuResultUrl, data=payload)
-    if(response.status_code >= 300):
-        return
-        
-    soup = BeautifulSoup(response.text, "html.parser")
-    resultTable = soup.find_all('table')[1]
-    tableRow = resultTable.find_all('tr')
-    for i in range(1,len(tableRow)):
-        tr = tableRow[i]
+    @staticmethod
+    def isValidRollNumber(rollNum: str = None) -> str:
+        if(len(rollNum) != 10):
+            raise ValueError("Roll-Number not 10 digit")
         try:
-            currentCol = tr.text.split('\n')[1:-1]
-            resultDict[i-1] = {
-                'SUB_CODE': currentCol[0],
-                'SUB_NAME': currentCol[1],
-                'INTERNAL': currentCol[2],
-                'EXTERNAL': currentCol[3],
-                'TOTAL': currentCol[4],
-                'GRADE': currentCol[5],
-                'CREDIT': currentCol[6]
-            }
-        except Exception as e:
-            print(e)
-            break
+            int(rollNum[:2])
+            int(rollNum[-3:], base=16)
+        except ValueError:
+            raise ValueError("Roll-Number not Valid")
+        return rollNum
 
-    return resultDict
+    def get_url(self) -> str:
+        params = [
+            f"degree={self.degree}",
+            f"&examCode={self.examCode}",
+            f"&etype={self.eType}",
+            f"&type={self.out_type}",
+            f"&htno={self.rollNum}",
+            f"&result=null&grad=null"
+        ]
+        final = f'{self.SERVERS[0]}/resultAction?'+''.join(params)
+        print(final)
+        return final
+
+    @staticmethod
+    def gradeToPoints(grade: str) -> int:
+        if(grade == 'F' or grade == 'Ab'):
+            return 0
+        elif(grade == 'C'):
+            return 5
+        elif(grade == 'B'):
+            return 6
+        elif(grade == 'B+'):
+            return 7
+        elif(grade == 'A'):
+            return 8
+        elif(grade == 'A+'):
+            return 9
+        elif(grade == "O"):
+            return 10
+        else:
+            return 0
+
+    def SGPACalculator(self, result: dict) -> float:
+        if not result:
+            return
+        total_credit = 0
+        gpa_accum = 0
+        for item in result:
+            gpa_accum += self.gradeToPoints(item['GRADE']) * \
+                float(item['CREDIT'])
+            total_credit += float(item['CREDIT'])
+        return round(gpa_accum/total_credit, 2)
+
+    def JNTUResultAPI(self):
+        resultDict = []
+        response = requests.request("POST", self.url)
+        if(response.status_code >= 300):
+            return
+        soup = BeautifulSoup(response.text, "html.parser")
+        resultTable = soup.find_all('table')
+        nameRow = resultTable[0].find_all('b')
+        self.user = [nameRow[n+1].text for n in range(0, len(nameRow), 2)]
+        tableRow = resultTable[1].find_all('tr')
+        for i in range(1, len(tableRow)):
+            tr = tableRow[i]
+            try:
+                currentCol = tr.text.split('\n')[1:-1]
+                resultDict.append({
+                    'SUB_CODE': currentCol[0],
+                    'SUB_NAME': currentCol[1],
+                    'INTERNAL': currentCol[2],
+                    'EXTERNAL': currentCol[3],
+                    'TOTAL': currentCol[4],
+                    'GRADE': currentCol[5],
+                    'CREDIT': currentCol[6]
+                })
+            except Exception as e:
+                print(e)
+                break
+
+        return resultDict
 
 
 if __name__ == "__main__":
-    resultDict = JNTUResultAPI("XXXXXXXXXX")
-    print(resultDict)
-    print(GPACalculator(resultDict))
-
-
-    
+    result = JNTUResult("18P61A0469", 1454)
+    print(result())
